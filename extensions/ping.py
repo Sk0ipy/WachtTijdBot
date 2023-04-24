e951360ba1fd5f4239e25c8d83bc608a6fbb6dfd
Sk0ipy <74152292+Sk0ipy@users.noreply.github.com>
2023-04-24 15:44:48 +0200

    24/4
    school werk

---
+++ extensions/ping.py
@@ -0,0 +1,30 @@
+import lightbulb, hikari
+
+from config import *
+
+config = {
+    'TOKEN': token,
+    '!': prefix,
+    'OWNER': owner,
+    'NAME': name,
+    'VERSION': version,
+    'GUILD_ID': guild_id,
+
+}
+
+bot = lightbulb.BotApp(token=config['TOKEN'], intents=hikari.Intents.ALL, default_enabled_guilds=[config['GUILD_ID']])
+
+plugin = lightbulb.Plugin('ping')
+
+def load(bot):
+    bot.add_plugin(plugin)
+
+
+@plugin.command()
+@lightbulb.command('ping','ping command')
+@lightbulb.implements(lightbulb.SlashCommand)
+async def ping(ctx):
+    await ctx.respond('pong')
+
+
+
--- main.py
+++ main.py
@@ -1,7 +1,50 @@
-import hikari
-import lightbulb
+import discord
+from discord.ext import commands
+from discord.ui import *
 
 from config import *
+
+bot = commands.Bot(command_prefix="!", intents=discord.Intents.all())
+
+
+@bot.event
+async def on_ready():
+    print("Bot is ready")
+    try:
+        synced = await bot.tree.sync()
+        print(f"Synced {synced} commands")
+    except Exception as e:
+        print(f"Error syncing commands: {e}")
+
+
+@bot.tree.command(name="wachttijd", description="Get the waiting time for a country")
+async def wachttijd(interaction: discord.Interaction, country: str):
+    select = Select(
+        options=[
+            discord.SelectOption(label="Belgium", value="Belgium"),
+            discord.SelectOption(label="Brazil", value="Brazil"),
+            discord.SelectOption(label="Canada", value="Canada"),
+            discord.SelectOption(label="China", value="China"),
+            discord.SelectOption(label="Denmark", value="Denmark"),
+            discord.SelectOption(label="England", value="England"),
+            discord.SelectOption(label="France", value="France"),
+            discord.SelectOption(label="Germany", value="Germany"),
+            discord.SelectOption(label="Hong Kong", value="Honk Kong"),
+            discord.SelectOption(label="Italy", value="Italy"),
+            discord.SelectOption(label="Japan", value="Japan"),
+            discord.SelectOption(label="Mexico", value="Mexico"),
+            discord.SelectOption(label="Netherlands", value="Netherlands"),
+            discord.SelectOption(label="Poland", value="Poland"),
+            discord.SelectOption(label="South Korea", value="South Korea"),
+            discord.SelectOption(label="Spain", value="Spain"),
+            discord.SelectOption(label="Sweden", value="Sweden"),
+            discord.SelectOption(label="United States", value="United States"),
+        ]
+    )
+    view = View()
+    view.add_item(select)
+    await 
+
 
 config = {
     'TOKEN': token,
@@ -13,29 +56,4 @@ config = {
 
 }
 
-bot = lightbulb.BotApp(token=config['TOKEN'])
-
-
-@bot.listen(hikari.StartedEvent)
-async def on_started(event):
-    print("Bot is ready!")
-
-
-@bot.command
-@lightbulb.command('ping', 'shows the latency of the bot')
-@lightbulb.implements(lightbulb.SlashCommand)
-async def ping(ctx):
-    await ctx.respond(f'Pong! {round(bot.heartbeat_latency * 1000)}ms')
-
-@bot.command
-@lightbulb.option('number1', 'first number', type=int)
-@lightbulb.option('number2', 'second number', type=int)
-@lightbulb.command('add', 'adds two numbers')
-@lightbulb.implements(lightbulb.SlashCommand)
-async def add(ctx):
-    await ctx.respond(
-        f'{ctx.options["number1"]} + {ctx.options["number2"]} = {ctx.options["number1"] + ctx.options["number2"]}')
-
-
-
-bot.run()
+bot.run(config['TOKEN'])
