ac32126ee197a93393d8c6e4602f30b05e771474
Sk0ipy <74152292+Sk0ipy@users.noreply.github.com>
2023-04-25 11:42:24 +0200

    25/4
    opschool:
    
    probleem met land/rides

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from discord.ui import *
 from config import *
 from parksPcountry import get_parks_per_country
 from parksPcountry import rides_per_park
+from parksPcountry import wait_time
 
 """
 get all the parks per country with help of the json file
@@ -34,13 +35,9 @@ async def help(ctx):
                     "laat zien",
         color=0x00ff00)
     embed.add_field(name="!help", value="Laat dit bericht zien", inline=False)
-    embed.add_field(name="!wt", value="Laat de wachttijd van een station zien", inline=False)
-    embed.add_field(name="!wtcountry", value="Laat de wachttijd van een land zien", inline=False)
-    embed.add_field(name="!clear", value="Verwijderd alle berichten", inline=False)
-    embed.add_field(name="!ping", value="Laat de ping zien", inline=False)
-    embed.add_field(name="!version", value="Laat de versie zien", inline=False)
-    embed.add_field(name="!owner", value="Laat de eigenaar zien", inline=False)
-    embed.add_field(name="!name", value="Laat de naam zien", inline=False)
+    embed.add_field(name="!getrides", value="Laat alle attracties zien van de gekozen pretpark", inline=False)
+    embed.add_field(name="!gettimes", value="Laat alle wachttijden zien per pretpark", inline=False)
+
     await ctx.send(embed=embed)
 
 
@@ -50,7 +47,7 @@ this is the !wtcountry, this will get the coutry that user want to see
 
 
 @bot.command()
-async def getwt(ctx):
+async def getrides(ctx):
     select_country = Select(
         placeholder="Selecteer een land",
         options=[
@@ -86,12 +83,84 @@ async def getwt(ctx):
         async def park_callback(interaction):
             park = interaction.data["values"][0]
             park_rides = rides_per_park(park, country)
+
             embed = discord.Embed(
                 title=f"{park} rides",
                 description="Here are the rides in this park",
                 color=0x00ff00
             )
             embed.add_field(name="Rides", value="\n".join(park_rides), inline=False)
+            await interaction.response.send_message(embed=embed)
+
+        select_park.callback = park_callback
+        view = View()
+        view.add_item(select_park)
+        await interaction.response.edit_message(view=view)
+        return
+
+    # add other countries' logic here
+
+    select_country.callback = country_callback
+    view = View()
+    view.add_item(select_country)
+    await ctx.send("Select a country", view=view)
+
+
+@bot.command()
+async def gettimes(ctx):
+    select_country = Select(
+        placeholder="Selecteer een land",
+        options=[
+            discord.SelectOption(label="Belgium", value="Belgium"),
+            discord.SelectOption(label="Brazil", value="Brazil"),
+            discord.SelectOption(label="Canada", value="Canada"),
+            discord.SelectOption(label="China", value="China"),
+            discord.SelectOption(label="Denmark", value="Denmark"),
+            discord.SelectOption(label="England", value="England"),
+            discord.SelectOption(label="France", value="France"),
+            discord.SelectOption(label="Germany", value="Germany"),
+            discord.SelectOption(label="Hong Kong", value="Honk Kong"),
+            discord.SelectOption(label="Italy", value="Italy"),
+            discord.SelectOption(label="Japan", value="Japan"),
+            discord.SelectOption(label="Mexico", value="Mexico"),
+            discord.SelectOption(label="Netherlands", value="Netherlands"),
+            discord.SelectOption(label="Poland", value="Poland"),
+            discord.SelectOption(label="South Korea", value="South Korea"),
+            discord.SelectOption(label="Spain", value="Spain"),
+            discord.SelectOption(label="Sweden", value="Sweden"),
+            discord.SelectOption(label="United States", value="United States"),
+        ]
+    )
+
+    async def country_callback(interaction):
+        country = interaction.data["values"][0]
+        parks = get_parks_per_country(country)
+
+        select_park = Select(
+            placeholder="Selecteer een pretpark",
+            options=[discord.SelectOption(label=park, value=park) for park in parks]
+        )
+
+        async def park_callback(interaction):
+            park = interaction.data["values"][0]
+            park_rides = wait_time(park, country)
+
+            name = []
+            time = []
+            status = []
+            for i in park_rides:
+                name.append(i[0])
+                time.append(i[1])
+                status.append(i[2])
+
+            embed = discord.Embed(
+                title=f"{park} rides",
+                description="Here are the rides in this park",
+                color=0x00ff00
+            )
+            embed.add_field(name="Rides", value="\n".join(name), inline=True)
+            embed.add_field(name="Time", value="\n".join(time), inline=True)
+            embed.add_field(name="Status", value="\n".join(status), inline=True)
             await interaction.response.send_message(embed=embed)
 
         select_park.callback = park_callback
--- parksPcountry.py
+++ parksPcountry.py
@@ -43,6 +43,7 @@ def rides_per_park(park_name, country):
 
         park = parks[0]
         park_id = park['id']
+        print(f"park ID: {park_id}")
         url = f"https://queue-times.com/nl/parks/{park_id}/queue_times.json"
         response = requests.get(url)
         data = response.json()
@@ -53,5 +54,157 @@ def rides_per_park(park_name, country):
 
         ride_names = [ride["name"] for ride in rides]
         return ride_names
+
+
+"""
+this is a example of the json file that i get from the api per park
+
+{
+    "id":5585,
+    "name":"#LikeMe Coaster",
+    "is_open":true,
+    "wait_time":0,
+    "last_updated":"2023-04-24T13:01:13.000Z"
+},
+
+get waitime and status of the ride
+"""
+
+
+def wait_time(park_name, country):
+    with open('data.json') as f:
+        data = json.load(f)
+        parks = data[country]
+        parks = [park for park in parks if park["name"] == park_name]
+
+        if not parks:
+            return "Park not found"
+
+        park = parks[0]
+        park_id = park['id']
+        url = f"https://queue-times.com/nl/parks/{park_id}/queue_times.json"
+        response = requests.get(url)
+        data = response.json()
+
+        # check if the park has rides in the rides or land key
+        if data.get("lands"):
+            lands = data.get("lands")
+            # get all the names of the lands
+            land_names = [land["name"] for land in lands]
+            # print the names of rides in the lands
+            for land in lands:
+                rides = land.get("rides")
+
+            # all rides = land + rides
+            all_rides = []
+            all_wait_time = []
+            all_is_open = []
+            for land in lands:
+                rides = land.get("rides")
+                all_rides += rides
+                for ride in rides:
+                    all_wait_time.append(ride["wait_time"])
+                    all_is_open.append(ride["is_open"])
+
+
+
+            # change the status from true/false to open/closed
+            for i in range(len(all_is_open)):
+                if all_is_open[i] == True:
+                    all_is_open[i] = "Open"
+                else:
+                    all_is_open[i] = "Closed"
+
+            all_rides = [str(i) for i in all_rides]
+            # change the wait time from int to string
+            all_wait_time = [str(i) for i in all_wait_time]
+
+            # add min to the wait time
+            for i in range(len(all_wait_time)):
+                all_wait_time[i] += " min"
+
+            # make a list with all the info
+            total_info = []
+            # print(len(all_rides))
+            for i in range(0, len(all_rides)):
+                print(i)
+                total_info.append([all_rides[i], all_wait_time[i], all_is_open[i]])
+
+            return total_info
+
+
+
+        elif data.get("rides"):
+            rides = data.get("rides")
+            ride_names = [ride["name"] for ride in rides]
+            is_open = [ride["is_open"] for ride in rides]
+            wait_time = [ride["wait_time"] for ride in rides]
+
+
+
+            wait_time = [str(i) for i in wait_time]
+
+            for i in range(len(wait_time)):
+                wait_time[i] += " min"
+
+            # change the status from true/false to open/closed
+            for i in range(len(is_open)):
+                if is_open[i] == True:
+                    is_open[i] = "Open"
+                else:
+                    is_open[i] = "Closed"
+
+            total_info = []
+            for i in range(len(rides)):
+                total_info.append([ride_names[i], wait_time[i], is_open[i]])
+
+        else:
+            return "No rides or lands found for this park"
+
+        return total_info
+
+
+def a(park_name, country):
+    with open('data.json') as f:
+        data = json.load(f)
+        parks = data[country]
+        parks = [park for park in parks if park["name"] == park_name]
+
+        if not parks:
+            return "Park not found"
+
+        park = parks[0]
+        park_id = park['id']
+        url = f"https://queue-times.com/nl/parks/{park_id}/queue_times.json"
+        response = requests.get(url)
+        data = response.json()
+
+        rides = data.get("rides")
+        if not rides:
+            return "No rides found for this park"
+
+        # get the wait time and status of all the rides
+        ride_names = [ride["name"] for ride in rides]
+        ride_wait_time = [ride["wait_time"] for ride in rides]
+        ride_status = [ride["is_open"] for ride in rides]
+
+
+        ride_wait_time = [str(i) for i in ride_wait_time]
+
+        # change the status from true/false to open/closed
+        for i in range(len(ride_status)):
+            if ride_status[i] == True:
+                ride_status[i] = "Open"
+            else:
+                ride_status[i] = "Closed"
+
+        total_info = []
+
+
+
+        for i in range(len(rides)):
+            total_info.append([ride_names[i], ride_wait_time[i], ride_status[i]])
+
+        return total_info
 
 
