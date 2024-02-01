af91b5f53561044f73a01d1ee94d14a07b2b4773
Sk0ipy <74152292+Sk0ipy@users.noreply.github.com>
2024-02-01 09:26:02 +0100

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 # this file will be used to configure the application
 
 # discord bot token
-main_token = "MTEwMDA5ODg5OTU3MTY0NjU5NA.GtwUWT.otXsiJ70zxdGTCwxCxFwDJ6sHSM-TrG_Ih7mAs"
-efteling_token = "MTEwMDgwMDA4MDgzMjkwOTM1Mg.GYMU--.qC6RM77TGi5CRMZz7ezadu1SYIgVy7d6uJi5Dc"
+main_token = "-"
+efteling_token = "-"
 
 # guild id
 guild_id = "1059189027611349002"
